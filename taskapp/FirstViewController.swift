5eeaa615c529b81081c2bf975660dc064ee0da3b
taku00jp <saitou@saitou-no-imac.local>
2016-04-04 20:00:01 +0900

    追加

--- taskapp/FirstViewController.swift
+++ taskapp/FirstViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = [String]()
+
+class FirstViewController: UIViewController, UITableViewDelegate{
+
+    
+    @IBOutlet var toDolist: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,45 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return toDoList.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            toDoList.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            
+         toDoListTable.reloadData()
+            
+        }
+        
+        
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+       toDoListTable.reloadData()
+        
+    }
+    
+    
+    
 }
 
--- taskapp/SecondViewController.swift
+++ taskapp/SecondViewController.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var item: UITextField!
+  
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(item.text!)
+        
+        item.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+     
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
 
 
 }
